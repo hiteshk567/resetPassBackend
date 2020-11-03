577e8742f8198b2aed98b5f8ea236c8100595ccf
Lenovo <hiteshk567@gmail.com>
2020-11-03 23:39:41 +0530

    cors

--- app.js
+++ app.js
@@ -24,16 +24,7 @@ const User = mongoose.model("User", userSchema);
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-
-  next();
-});
+app.use(cors());
 app.use(bodyParser.json());
 
 app.post("/user/login", async (req, res) => {
